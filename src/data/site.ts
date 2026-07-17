6f7a5d2ad0a23523e7ad98884337723257d19302
ziondigitallabs-zdl <ziondigitallabs@gmail.com>
2026-07-17 14:16:35 +0530

    Create src/data/site.ts

---
+++ src/data/site.ts
@@ -0,0 +1,27 @@
+export interface SiteConfig {
+  phone1: string;
+  phone2: string;
+  whatsappNumber: string;
+  email: string;
+  rating: number;
+  reviewCount: number;
+  address: string;
+  mapsUrl: string;
+  instagram: string;
+  facebook: string;
+  youtube: string;
+}
+
+export const siteConfig: SiteConfig = {
+  phone1: "+91 73833 12356",
+  phone2: "+91 96244 04030",
+  whatsappNumber: "917383312356",
+  email: "panchalcars@gmail.com",
+  rating: 4.2,
+  reviewCount: 31,
+  address: "Manjalpur, Vadodara",
+  mapsUrl: "https://www.google.com/maps/dir//Panchal+Cars,+society+Nr,+Parth+Bhoomi+1,+Jupiter+Rd,+Manjalpur,+Vadodara,+Gujarat+390010/@23.0260736,72.5188608,11z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x395fc5b056ec33cd:0x202a5f88a4c56985!2m2!1d73.1944263!2d22.2614461?entry=ttu&g_ep=EgoyMDI2MDcxMy4wIKXMDSoASAFQAw%3D%3D",
+  instagram: "https://www.instagram.com/panchalcars/?hl=en",
+  facebook: "https://www.facebook.com/profile.php?id=100066388579343",
+  youtube: "https://www.youtube.com/@panchalcars"
+};
